63fe7d297c5cbf1ef4c122d35913929e1733e91a
Bob Kozdemba <bkozdemba@gmail.com>
2018-07-22 12:23:05 -0500

    updated

--- webserver.sh
+++ webserver.sh
@@ -3,3 +3,7 @@
 # LetsEncrypt testing.
 #
 docker run -it --rm --name certbot docker.io/certbot/certbot certonly --email bkozdemba@gmail.com -d presto.eadgbe.net --standalone --agree-tos --dry-run
+
+#
+# https://www.tenormanmike.com/website-and-hosting-issues/how-to-install-a-lets-encrypt-ssl-on-a-shared-godaddy-hosting-account/
+#
