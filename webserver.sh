0b8e9b0bc79e1cfb5e661b34500170a53bee2900
Bob Kozdemba <bkozdemba@gmail.com>
2018-07-22 20:39:51 -0500

    updated

--- webserver.sh
+++ webserver.sh
@@ -8,5 +8,5 @@ docker run -it --rm --name certbot docker.io/certbot/certbot certonly --email bk
 # https://www.tenormanmike.com/website-and-hosting-issues/how-to-install-a-lets-encrypt-ssl-on-a-shared-godaddy-hosting-account/
 #
 # Easy way to create a cert for an existing web server.
-#
+# To renew, copy and paste in the CSR.
 # https://zerossl.com/
